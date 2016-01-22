acf9f701b6dcd640197663cb1641b52bc5c4a08d
David Broman <dbro@kth.se>
2016-01-22 17:39:25 +0100

    Removed some files.:

--- applications/data_logger/build/default/production/_ext/710032201/main.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/_ext/710032201/main.o:  \
- ../../morap/applications/data_logger/main.c  \
- ../../morap/applications/data_logger/main_declarations.h  \
- ../../morap/applications/data_logger/../../api/pic32/modular_arms.h  \
- ../../morap/applications/data_logger/../../api/pic32/pic32_i2c.h  \
- ../../morap/applications/data_logger/logging_routines.h 
--- applications/data_logger/build/default/production/_ext/710032201/main_declarations.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/_ext/710032201/main_declarations.o:  \
- ../../morap/applications/data_logger/main_declarations.c  \
- ../../morap/applications/data_logger/main_declarations.h 
